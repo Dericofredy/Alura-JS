29aa5b4eb02e09474f015251fbb240a95243366d
Frederico Caldeira <60188689+Dericofredy@users.noreply.github.com>
2024-11-08 16:58:40 -0300

    usando função anonima

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-function tocaSomPom(){
-        document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementaudio){
+        document.querySelector(idElementaudio).play(); /*parametro id tag audio */
 }
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-while (contador<9){
-        listaTeclas[contador].onclick = tocaSomPom;
+while (contador<listaTeclas.length) {
+        listaTeclas[contador].onclick = function(){ /* função anonima  */
+                tocaSom('#som_tecla_pom');
+        };
 
         contador = contador+1;
         console.log(contador);
