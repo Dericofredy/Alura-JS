cce70e00e4b39308c23aca1325b63eba42c68584
Frederico Caldeira <60188689+Dericofredy@users.noreply.github.com>
2024-11-08 16:41:54 -0300

    funciona, mas todos terá o som da tecla Pom

--- main.js
+++ main.js
@@ -2,5 +2,13 @@ function tocaSomPom(){
         document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick=tocaSomPom;
+const listaTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+while (contador<9){
+        listaTeclas[contador].onclick = tocaSomPom;
+
+        contador = contador+1;
+        console.log(contador);
+}
