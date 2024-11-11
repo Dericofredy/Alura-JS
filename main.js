44af49b316e9e7c914b9c9092d6074cd39379333
Frederico Caldeira <60188689+Dericofredy@users.noreply.github.com>
2024-11-11 12:47:21 -0300

    Minimizando código : const tecla = listaDeTeclas[contador]; uso de template string e idAudio

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ let contador = 0;
 
 while (contador<listaDeTeclas.length) {
 
-        instrumento = listaDeTeclas[contador].classList[1];
+        const tecla = listaDeTeclas[contador];
+        
+        instrumento = tecla.classList[1];
         console.log(instrumento);
+        //Template String
+        const idAudio = `#som_${instrumento}`;
+        console.log(idAudio);
 
-        listaDeTeclas[contador].onclick = function(){ /* função anonima  */
+        tecla.onclick = function(){ /* função anonima  */
                 tocaSom('#som_tecla_pom');
         };
 
