1d559580d2d2b753972263f20052c18056dad75c
Frederico Caldeira <60188689+Dericofredy@users.noreply.github.com>
2024-11-12 12:13:41 -0300

    removendo ação de precionar vermelho

--- main.js
+++ main.js
@@ -34,8 +34,14 @@ for (contador =0; contador <listaDeTeclas.length; contador++){
 
         tecla.onclick = function (){
                 tocaSom(idAudio);
-        }  
+        } 
+
         tecla.onkeydown = function (){
                 tecla.classList.add('ativa');
-        }             
+        } 
+        
+        tecla.onkeyup = function () {
+                tecla.classList.remove('ativa');
+        }
+
 }
